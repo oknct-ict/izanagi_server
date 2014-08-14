0908dd165cd77bc4970919036ab76ae150e056c3
marin72-com <sifon1994@gmail.com>
2014-08-14 13:01:56 +0000

    IDEなしでもtestができるように、test環境を作った。

--- app.py
+++ app.py
@@ -14,7 +14,11 @@ app = Flask(__name__);
 
 @app.route('/')
 def index():
-    return render_template('test.html');
+    return render_template('index.html');
+
+@app.route("/test")
+def test():
+    return render_template("test.html");
 
 @app.route('/websock/ide/')
 def websock_ide():
