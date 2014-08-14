1171342af97625cfeaadc450c9eadbd12227c002
Orisano <weekendsummer.endless@gmail.com>
2014-08-14 17:23:10 +0900

    LoginのJSサイドの実装
    loginだけ実装していてほかは実装してない

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,58 @@
 (function() {
   $(function() {
-    var editor, ws;
+    var editor, make_msg, session_id, ws;
     editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
       lineNumbers: true,
       mode: "text/x-vb",
       matchBrackets: true
     });
-    ws = new WebSocket("ws://nado.oknctict.tk:5000/echo");
-    return $("#btn-upload").click(function() {
+    ws = new WebSocket("ws://nado.oknctict.tk:5000/websock/ide");
+    session_id = null;
+    ws.onclose = ws.onerror = function(error) {
+      return alert("please reload web page");
+    };
+    ws.onmessage = function(event) {
       var msg;
-      msg = {
+      msg = event.data;
+      console.log(msg);
+      if (msg.command === "login_RES") {
+        if (msg.data.result < 100) {
+          session_id = msg.session_id;
+          return $("#loginModal").modal("hide");
+        } else {
+          return $("#loginModal").modal("show");
+        }
+      }
+    };
+    make_msg = function(command, data) {
+      var sid;
+      if (session_id === null) {
+        sid = "";
+      } else {
+        sid = session_id;
+      }
+      return JSON.stringify({
         type: "ide",
-        command: "SYN",
-        message: editor.getValue()
-      };
-      return ws.send(JSON.stringify(msg));
+        session_id: sid,
+        command: command + "_REQ",
+        data: data
+      });
+    };
+    $("#btn-login").click(function() {
+      var msg, passwd, user_id;
+      user_id = $("#user_id").val();
+      passwd = $.sha256($("#passwd"));
+      msg = make_msg("login", {
+        user_id: user_id,
+        password: passwd
+      });
+      console.log(msg);
+      return ws.send(msg);
+    });
+    return $("#btn-upload").click(function() {
+      if (session_id === null) {
+        $("#loginModal").modal("show");
+      }
     });
   });
 
