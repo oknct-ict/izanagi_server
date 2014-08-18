28e75a6f9d15cbe6aac80c4182db11e7b80be513
marin72-com <sifon1994@gmail.com>
2014-08-18 06:05:09 +0000

    register関数に関数ヘッダーをつけた。userからuser_idに変数名を変えた

--- server/check_input.py
+++ server/check_input.py
@@ -10,6 +10,14 @@ GRADE   = "grade"
 LEN         = 0;
 LEN_ALPHA   = 1;
 
+'''
+文字列が正しいか（文字列の長さや、英数字のみ）かを判定する
+@param string
+@param str_len
+@param case
+@return 正しい：0、不正：エラーコード
+
+'''
 def is_correct_str(string, str_len, case):
     # length check
     if len(string) > str_len or len(string) == 0:
@@ -22,6 +30,14 @@ def is_correct_str(string, str_len, case):
             return myconst.DATA_NON_REGULATED;
     return myconst.OK
 
+'''
+ユーザー登録情報が正しいか
+@param data
+@return user_id     ユーザーID
+@return password    パスワード
+@return mail        メールアドレス
+@return grade       学年
+'''
 def register(data):
     print USER in data;
     print PASS in data;
@@ -33,16 +49,16 @@ def register(data):
         GRADE not in data:
         print "tarinai";
         return ("", "", "", "", myconst.DATA_DEFICIENCY);
-    user = data[USER];
+    user_id = data[USER];
     password = data[PASS];
     mail = data[MAIL];
     grade = data[GRADE];
-    if is_correct_str(user, 16, LEN_ALPHA) != myconst.OK or \
+    if is_correct_str(user_id, 16, LEN_ALPHA) != myconst.OK or \
         is_correct_str(password, 32, LEN_ALPHA) != myconst.OK or \
         is_correct_str(mail, 64, LEN) != myconst.OK:
         print "nagasatoka";
         return ("", "", "", "", myconst.DATA_NON_REGULATED);
-    return (user, password, mail, grade, myconst.OK);
+    return (user_id, password, mail, grade, myconst.OK);
 
 
 
